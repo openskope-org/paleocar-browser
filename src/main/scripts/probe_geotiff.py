a19a13eb7f63624eb94fab4f1c2c334a6d726d1c
Timothy McPhillips <tmcphillips@absoluteflow.org>
2016-12-15 19:28:23 -0800

    Reorganized probe script.

--- src/main/scripts/probe_geotiff.py
+++ src/main/scripts/probe_geotiff.py
@@ -2,65 +2,62 @@
 
 import optparse
 import struct
-
+from enum import Enum
 from osgeo import gdal
 from gdalconst import GA_ReadOnly
 
+class RasterDataType(Enum): (UNKNOWN, BYTE, UINT16, INT16, 
+                             UINT32, INT32, FLOAT32, FLOAT64, 
+                             CINT16, CINT32, CFLOAT32, CFLOAT64) = range(12)
+
 def probe_geotiff(geotiff_filename):
 
-    # get read-only access to the dataset in the file
+    # get read-only access to the dataset
     dataset = gdal.Open(geotiff_filename, GA_ReadOnly)
 
-    # print GDAL driver information
+    #
     driver = dataset.GetDriver()
-    print('')
-    print('Driver: {}'.format(driver.LongName))
-    print('')
-
-    print("***** Dataset *****")
-    geotransform = dataset.GetGeoTransform()
     projection = dataset.GetProjection()
-    print('Projection:  {}'.format(projection))
+    geotransform = dataset.GetGeoTransform()
     origin_x = geotransform[0]
     origin_y = geotransform[3]
-    print('Origin:      {}, {} '.format(origin_x, origin_y))
-    pixel_size_x = geotransform[1]
-    pixel_size_y = - geotransform[5]
+    pixel_size_x = abs(geotransform[1])
+    pixel_size_y = abs(geotransform[5])
     pixels_per_degree_x = int(1.0 / pixel_size_x)
     pixels_per_degree_y = int(1.0 / pixel_size_y)
-    print('Pixel size:  {:f} x {:f}  (1/{:d} x 1/{:d})'.format(
-        pixel_size_x, pixel_size_y, pixels_per_degree_x, pixels_per_degree_y))
 
-    # print dataset dimensions
-    x_size = dataset.RasterXSize 
-    y_size = dataset.RasterYSize 
-    print('Raster size: {} x {} ({:f} deg x {:f} deg)'.format(x_size, y_size, x_size * pixel_size_x, y_size * pixel_size_y))
-    bands = dataset.RasterCount
-    print('Bands:       {}'.format(bands))
-
-#    print('Dataset size:  {}pixels x {}pixels x {} bands ({:f} deg)').format(x_size,  x_size * pixel_size_x)
-#    print('Y-size:  {} pixels ({:f} deg)').format(y_size)
-    print('')
-
-    # print band 1 information
-
-    print('***** Band 1 *****')
+    # get dataset dimensions
+    x_size = dataset.RasterXSize
+    y_size = dataset.RasterYSize
+    band_count = dataset.RasterCount
     band = dataset.GetRasterBand(1)
     blocksize_x, blocksize_y= band.GetBlockSize()
     nodata_value = band.GetNoDataValue()
-    print('Block size:     {}x{}'.format(blocksize_x, blocksize_y))
-    print('No-data value:  {}'.format(nodata_value))
-    print(band.DataType)
-    print('')
 
-    metadata = band.GetMetadataItem('TIFFTAG_GDAL_NODATA')
-    print(metadata)
+    print('Driver            {}'.format(driver.LongName))
+    print('Origin            {}, {} '.format(origin_x, origin_y))
+    print('Pixel size (deg)  {:f} x {:f}  (1/{:d} x 1/{:d})'
+        .format(pixel_size_x, pixel_size_y, pixels_per_degree_x, pixels_per_degree_y))
+    print('Raster size (px)  {} x {} '.format(x_size, y_size))
+    print('Raster size (deg) {:f} x {:f}'.format(x_size * pixel_size_x, y_size * pixel_size_y))
+    print('Number of bands   {}'.format(band_count))
+    print('Block size        {} x {}'.format(blocksize_x, blocksize_y))
+    print('No-data value     {}'.format(nodata_value))
+    print('Pixel data type   {}'.format(RasterDataType(band.DataType).name))
 
-    band_data = dataset.ReadRaster(xsize=blocksize_x, ysize=1)
-    print(len(band_data))
+    min = band.GetMinimum()
+    max = band.GetMaximum()
+    if min is None or max is None:
+        (min,max) = band.ComputeRasterMinMax(1)
 
-    band = dataset.GetRasterBand(2000)
+    print('Minimum value     {}'.format(min))
+    print('Maximum value     {}'.format(max))
 
+
+    # band_data = dataset.ReadRaster(xsize=blocksize_x, ysize=1)
+    # print(len(band_data))
+
+    # band = dataset.GetRasterBand(2000)
 
 if __name__ == '__main__':
     
